0024e6a07f70f56b1fb18481fd6fa0126fcdb3a5
elham34 <elham.alwadie@gmail.com>
2023-10-05 12:38:04 +0300

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Elham on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
